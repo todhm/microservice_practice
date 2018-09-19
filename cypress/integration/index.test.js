047881548b4a7bdb0c4ce74ddf9bb61c459753fc
fidel <fidel@keuleonchiui-MacBook-Pro.local>
2018-09-20 07:11:02 +0900

    add travis test

---
+++ cypress/integration/index.test.js
@@ -0,0 +1,30 @@
+describe('Index',()=>{
+    it('users should be able to view the "/ page',()=>{
+        cy
+          .visit('/')
+          .get('h1').contains('All Users');
+    })
+    context('Browser',()=>{
+        beforeEach(()=>{
+            cy.viewport('macbook-11')
+        })
+        it('You should be able to click new page',()=>{
+          cy
+            .visit('/')
+            .get('strong[class="navbar-item"]').click();
+        })  
+        
+    })
+    context('mobile',()=>{
+        beforeEach(()=>{
+            cy.viewport('iphone-6+')
+        })
+        it('You should be able to view burger',()=>{
+          cy
+            .visit('/')
+            .get('.navbar-burger').get('span');
+        })  
+        
+    })
+
+})
--- test.sh
+++ test.sh
@@ -16,6 +16,13 @@ docker-compose run client npm test -- --coverage
 inspect $? client 
 docker-compose down 
 
+docker-compose -f docker-compose-prod.yml up -d --build
+docker-compose -f docker-compose-prod.yml run users python manage.py recreate_db
+./node_modules/.bin/cypress run --config baseUrl=http://localhost
+inspect $? e2e 
+docker-compose -f docker-compose-prod.yml down 
+
+
 #return proper code 
 if [ -n "${fails}" ]; then 
     echo "Tests failed: ${fails}"
