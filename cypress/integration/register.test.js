0aba9be0d3ec80711d2d39116e271d29eeaf0e6d
fidel <fidel@keuleonchiui-MacBook-Pro.local>
2018-09-20 07:40:15 +0900

    add cypress dependencies

---
+++ cypress/integration/register.test.js
@@ -0,0 +1,36 @@
+const randomstring = require('randomstring');
+const username = randomstring.generate();
+const email = `${username}@test.com`;
+describe('Register',()=>{
+    it('should display the registration form',()=>{
+        cy 
+        .visit('/register')
+        .get('h1').contains('Register')
+        .get('form');
+    });
+
+    it('should allow a user to register',()=>{
+
+        //register user 
+        cy 
+        .visit('/register')
+        .get('input[name="username"]').type(username)
+        .get('input[name="email"]').type(email)
+        .get('input[name="password"]').type('test12345!@#$')
+        .get('input[type="submit"]').click() 
+
+
+        cy.contains('All Users');
+        cy.contains(username);
+        cy.get('.navbar-burger').click();
+        cy.get('.navbar-menu').withiin(()=>{
+            cy
+                .get('.navbar-item').contains('User Status')
+                .get('.navbar-item').contains('Log Out')
+                .get('.navbar-item').contains('Log In').should('not.be.visible')
+                .get('.navbar-item').contains('Register').should('not.be.visible');
+        })
+
+
+    })
+})
