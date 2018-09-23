e52cbdb5a339c2a2bec8e20bd11e3b6002cd9b3e
fidel <fidel@keuleonchiui-MacBook-Pro.local>
2018-09-23 18:08:40 +0900

    add test case in App.test.js

--- services/client/src/__tests__/App.test.js
+++ services/client/src/__tests__/App.test.js
@@ -1,14 +1,20 @@
 import React from "react";
-import { shallow } from "enzyme";
+import { shallow,mount } from "enzyme";
+import {MemoryRouter as Router} from 'react-router-dom';
 
 import App from "../App";
 beforeAll(()=>{
   global.localStorage={
     getItem:()=>'someToken',
     authToken:''
-
   };
 });
 test("App renders without crashing", () => {
   const wrapper = shallow(<App />);
 });
+test("App will call compoentWillMount when mounted",()=>{
+  const onWillMount = jest.fn(); 
+  App.prototype.componentWillMount = onWillMount; 
+  const wrapper = mount(<Router><App/></Router>);
+  expect(onWillMount).toHaveBeenCalledTimes(1);
+})
